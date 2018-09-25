eddf236ec1a883bc2bb8993a815ff5fa15fc1acf
Michael De Jesus <michaeldejesus2@Michaels-MacBook-Pro-2.local>
2018-09-25 16:05:57 -0300

    Test get todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,8 +5,15 @@ const {app} = require('./../server');
 
 const {Todo} = require('./../models/todo');
 
-beforeEach( (done) => {
-    Todo.remove({}).then(() => done());
+const todos = [{
+    text: 'first todo insert',
+}, {
+    text: 'second todo insert'
+}]
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos).then(() => done())
+    });
 });
 describe('Post /Todos', () => {
     it('Should create a new todo', (done) => {
@@ -22,7 +29,7 @@ describe('Post /Todos', () => {
             if (err) {
                 return done(err);
             }
-            Todo.find().then((todos) =>{
+            Todo.find({text}).then((todos) =>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -39,9 +46,21 @@ describe('Post /Todos', () => {
                 return done(err);
             }
             Todo.find().then((todos) =>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch(e => done(e));
         });
+    });
+});
+
+describe('Get /Todos', () =>{
+    it('Should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+               expect(res.body.todos.length).toBe(2);
+            })
+            .end(done)
     });
 });
